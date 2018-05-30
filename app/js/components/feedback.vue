770df9f06c832028eef44b72d1ffa7e934772931
Tim Stallmann <tim@stallmann.mayfirst.org>
2018-05-30 09:32:47 -0400

    Fix styling on feedback buttons

--- app/js/components/feedback.vue
+++ app/js/components/feedback.vue
@@ -1,7 +1,9 @@
 <template lang="html">
-       <div v-if="privateState.feedbackUrl || privateState.signupEmbed" class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop feedback">
+       <div v-if="privateState.feedbackUrl || privateState.signupEmbed" class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
+              <div class="feedback-buttons">
               <button class="mdl-button mdl-js-button mdl-button--raised" v-if="privateState.feedbackUrl" v-on:click="openSurvey">Share feedback or questions</button>
               <button v-bind:class="privateState.showSignup ? 'is-active' : ''" class="mdl-button mdl-js-button mdl-button--raised" v-if="privateState.signupEmbed" v-on:click="privateState.showSignup = !privateState.showSignup">Join our mailing list</button>
+              </div>
               <div v-if="privateState.showSignup" v-html="privateState.signupEmbed" class="signup-embed"></div>
        </div>
 </template>
@@ -18,8 +20,17 @@ export default {
 </script>
 
 <style lang="css" scoped>
+       .feedback-buttons {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+       }
        button {
               margin: 10px;
+              height: auto;
+              line-height: 1.5em;
+              padding: 5px;
+              flex: auto;
        }
        .signup-embed {
               margin: 10px;
