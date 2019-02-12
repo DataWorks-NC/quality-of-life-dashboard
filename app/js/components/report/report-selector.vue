9c30cf98ce2a87a309f25255a73b698d73c2330a
Tim Stallmann <tim@stallmann.mayfirst.org>
2019-02-12 11:11:49 -0500

    Tweak styling more

--- app/js/components/report/report-selector.vue
+++ app/js/components/report/report-selector.vue
@@ -1,33 +1,51 @@
 <template>
-  <div class="page page-category metric-selector d-print-none">
-    <div class="row">
+  <div :class="collapsed ? 'collapsed' : ''" class="page page-category metric-selector d-print-none">
+    <div class="row metric-selector__header">
       <div class="col-xs-12">
         <h3 class="metric-selector__title" @click="collapsed=!collapsed">Customize this report</h3>
       </div>
     </div>
-    <div v-if="!collapsed" class="row metric-selector__body">
+    <div v-if="!collapsed">
+    <div class="row metric-selector__body">
       <div class="col-xs-12">
         <p>Click on the title of a category to hide or show all of the metrics in that category. Click on the title of an individual metric to hide or show just that metric.</p>
       </div>
-      <div v-for="(category, i) in categories" class="col-xs-12 col-sm-6 col-md-4">
-        <div :key="i" class="list-group">
-          <a :class="category.visible ? 'active' : ''" class="list-group-item list-group-item-action" @click.prevent="toggleCategory(i)">{{ category.name }}</a>
+    </div>
+      <div class="row metric-selector__body">
+      <div v-for="category in hiddenCategories" class="col-xs-12 col-sm-6 col-md-4">
+        <div :key="category.name" class="list-group">
+          <a :class="category.visible ? 'active' : ''" class="list-group-item list-group-item-action" @click.prevent="toggleCategory(category.name)">{{ category.name }}</a>
+        </div>
+      </div>
+      </div>
+      <div class="row metric-selector__body">
+      <div v-for="category in visibleCategories" class="col-xs-12 col-sm-6 col-md-4">
+        <div :key="category.name" class="list-group">
+          <a :class="category.visible ? 'active' : ''" class="list-group-item list-group-item-action" @click.prevent="toggleCategory(category.name)">{{ category.name }}</a>
           <div v-if="category.visible" class="list-group metric-selector__sub-metrics">
             <a v-for="metric in category.metrics" :class="metric.visible ? 'active' : ''" class="list-group-item list-group-item-action metric-selector__sub-metric" @click.prevent="metric.visible = !metric.visible">{{ metric.title }}</a>
           </div>
         </div>
       </div>
+      </div>
     </div>
-  </div>
-  </div>
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'ReportSelector',
+  computed: {
+    visibleCategories() {
+      return this.categories.filter(c => c.visible);
+    },
+    hiddenCategories() {
+      return this.categories.filter(c => !c.visible);
+    },
+  },
   methods: {
-    toggleCategory(n) {
+    toggleCategory(categoryName) {
+      const n = this.categories.findIndex(c => c.name === categoryName);
       if (this.categories[n].visible) {
         this.categories[n].visible = false;
       } else {
@@ -41,7 +59,7 @@ export default {
 </script>
 
 <style scoped>
-  .metric-selector {
+  .collapsed {
     background: rgb(204, 204, 204);
   }
   .metric-selector__title {
@@ -49,11 +67,6 @@ export default {
     padding-left: 6px;
     color: #337ab7;
     cursor: pointer;
-  }
-  .metric-selector__body {
-    background: white;
-    border: 3px solid rgb(204,204,204);
-    padding: 6px;
   }
   .metric-selector__sub-metric {
     padding: 5px 15px;
