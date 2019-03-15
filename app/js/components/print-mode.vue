aa05e78eb4c75456ea13cd5bc72098f51bc931db
Tim Stallmann <tim@stallmann.mayfirst.org>
2019-03-15 10:25:39 -0400

    Add border

--- app/js/components/print-mode.vue
+++ app/js/components/print-mode.vue
@@ -1,13 +1,13 @@
 <template functional>
   <div class="mdl-grid">
     <print-map-header/>
-    <img src="./img/report-logo.png" class="header__logo">
+    <img src="./img/report-logo.png" alt="DataWorks NC logo" class="header__logo">
     <div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--12-col">
-      <div class="map-container" style="position: relative">
+      <main class="map-container" style="position: relative">
         <dashboard-map :mapbox-access-token="privateConfig.mapboxAccessToken" :map-config="mapConfig"/>
         <dashboard-legend/>
-      </div>
-      <footer style="padding:0.125in;">Map from the Durham Neighborhood Compass, a project of DataWorks NC. Visit <a href="https://compass.durhamnc.gov">compass.durhamnc.gov</a> to build your own map.</footer>
+      </main>
+      <footer>Map from the Durham Neighborhood Compass, a project of DataWorks NC. Visit <a href="https://compass.durhamnc.gov">the compass</a> to build your own map!</footer>
     </div>
   </div>
 </template>
@@ -45,13 +45,28 @@ export default {
   height: 8in;
 }
 
+.print footer {
+  font-size: 8pt;
+  padding: 0.125in;
+  font-style: italic;
+}
+
 .header__logo {
   display: none;
 }
 
 @media print {
-  .print .map-container, .print #map {
+  .print .map-container {
     height: 8in;
+  }
+
+  .print #map {
+    height: 8in;
+    border: 3pt double;
+  }
+
+  .print #legend {
+    margin: 3pt;
   }
 
   .header__logo {
