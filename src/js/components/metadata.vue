24c1bc599bf411a35514cf65f955467a1e6e84c9
alexmanzo <memanzo17@gmail.com>
2019-11-03 13:22:06 -0500

    Update format styles on footer, metadata additional resources

--- src/js/components/metadata.vue
+++ src/js/components/metadata.vue
@@ -16,47 +16,63 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Metadata',
-  computed: mapGetters(['metadataAbout', 'metadataImportant', 'metadataResources']),
+  name: "Metadata",
+  computed: mapGetters([
+    "metadataAbout",
+    "metadataImportant",
+    "metadataResources",
+  ]),
+  mounted() {
+    this.handleLinks();
+  },
   updated() {
     this.handleLinks();
   },
   methods: {
     handleLinks() {
-      const links = this.$el.getElementsByTagName('a');
+      const links = this.$el.getElementsByTagName("a");
       for (let i = 0; i < links.length; i += 1) {
-        links[i].innerHTML = `<span class="link-underline">${links[i].innerHTML}</span>`;
+        links[
+          i
+        ].innerHTML = `<span class="link-underline">${links[i].innerHTML}</span>`;
       }
     },
   },
 };
 </script>
 
-<style lang="css">
-.meta-table {
-    width: 100%;
-    white-space: normal;
-}
-.meta-table:first-of-type {
-    margin-bottom: 15px;
-}
-</style>
-
-<style lang="css" scoped>
+<style lang="scss">
 #metadata {
-    width: 100%;
-    padding: 0 10px;
+  width: 100%;
+  padding: 0 10px;
 }
 h3 {
-    margin-top: 45px;
+  margin-top: 45px;
 }
 h3:first-of-type {
-    margin-top: 0;
+  margin-top: 0;
 }
 h4 {
-    margin-top: 1em;
+  margin-top: 1em;
+}
+.meta-table {
+  width: 100%;
+  white-space: normal;
+
+  td {
+    padding-top: 10px;
+    padding-bottom: 10px;
+
+    &:first-child {
+      width: 25%;
+      padding-right: 15px;
+    }
+  }
+}
+.meta-table:first-of-type {
+  margin-bottom: 15px;
 }
 </style>
