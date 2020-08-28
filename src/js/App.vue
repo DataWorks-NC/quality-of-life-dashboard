c547f0125ec625cd59dedb148455705b86aaa145
Tim Stallmann <tim@rad.cat>
2020-08-28 08:12:48 -0400

    Re-introduce loading state

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app :style="{ backgroundColor: $vuetify.theme.themes['light'].background }">
-    <router-view />
+    <div v-if="loading">
+      <div style="width: 100%; height: 64px; background-color: rgb(66,66,66);" />
+      <div style="width: 100%; height: 600px;" />
+    </div>
+    <router-view v-else />
   </v-app>
 </template>
 
@@ -8,5 +12,11 @@
 
 export default {
   name: 'App',
+  data: () => ({
+    loading: true,
+  }),
+  mounted() {
+    this.loading = false;
+  },
 };
 </script>
