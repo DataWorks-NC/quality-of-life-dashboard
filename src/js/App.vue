4e93402562182f02de6e6d773a3f561200c108e0
Tim Stallmann <tim@rad.cat>
2020-08-28 07:51:59 -0400

    Re-add vue-i18n

--- src/js/App.vue
+++ src/js/App.vue
@@ -8,15 +8,5 @@
 
 export default {
   name: 'App',
-  beforeCreate() {
-    // Preload map resources so that they live on even between switching to Report and back.
-    // @see https://github.com/mapbox/mapbox-gl-js/pull/9391
-    import(/* webpackChunkName: "mapboxgl" */ 'mapbox-gl').then((mapboxgl) => {
-      mapboxgl.prewarm();
-      import(/* webpackChunkName: "mapboxgl" */ 'mapbox-gl/dist/mapbox-gl.css').then(() => {
-        this.$root.mapboxgl = mapboxgl;
-      });
-    });
-  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -161,6 +161,16 @@ const app = new Vue({
   el: '#app',
   vuetify,
   data: { mapboxgl: null },
+  beforeCreate() {
+    // Preload map resources so that they live on even between switching to Report and back.
+    // @see https://github.com/mapbox/mapbox-gl-js/pull/9391
+    import(/* webpackChunkName: "mapboxgl" */ 'mapbox-gl').then((mapboxgl) => {
+      mapboxgl.prewarm();
+      import(/* webpackChunkName: "mapboxgl" */ 'mapbox-gl/dist/mapbox-gl.css').then(() => {
+        this.$root.mapboxgl = mapboxgl;
+      });
+    });
+  },
   render: h => h(App),
 });
 
