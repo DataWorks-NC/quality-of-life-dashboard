451a35e17182cf6ffb5e3326d8509bd80d8af033
Tim Stallmann <tim@rad.cat>
2022-03-03 12:58:02 -0500

    New upgrades

--- build/export_translations.js
+++ build/export_translations.js
@@ -1,53 +1,54 @@
+/* eslint-disable no-console */
+
 // Convert translation files in javascript and english to a CSV file.
 // Each row contains a key, and then the translations for each language.
 
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
-const stringify = require('csv-stringify');
+const { stringify } = require('csv-stringify');
 
 // TODO: Handle data translation file path also.
 const translationFilePath = path.join(__dirname, '../src/locales/');
 const languages = ['en', 'es'];
 
-let output = {};
+const output = {};
 
 function iterateKeys(key, val, prefix) {
-  const newPrefix = prefix
-      ? (_.isNumber(key) ? `${prefix}[${key}]` : `${prefix}.${key}`)
-      : key;
+  let newPrefix = key;
+  if (prefix) {
+    newPrefix = _.isNumber(key) ? `${prefix}[${key}]` : `${prefix}.${key}`;
+  }
   if (_.isString(val)) {
     return { key: newPrefix, translation: val };
   }
-  else {
-    return _.flatMap(val, function(value, index) {
-      return iterateKeys(index, value, newPrefix);
-    });
-  }
+
+  return _.flatMap(val, (value, index) => iterateKeys(index, value, newPrefix));
 }
 
-for (const language of languages) {
-  const input = JSON.parse(fs.readFileSync(translationFilePath + language + '.json'));
+languages.forEach((language) => {
+  const input = JSON.parse(fs.readFileSync(`${translationFilePath + language}.json`));
 
-  _.forEach(iterateKeys(null, input, null), function(v) {
+  _.forEach(iterateKeys(null, input, null), (v) => {
     if (!_.has(output, v.key)) {
       output[v.key] = {};
     }
     output[v.key][language] = v.translation;
   });
-}
+});
 
 console.log(output);
 
 stringify(
-    _.map(output, (v, key) => _.merge({ "key": key }, v)),
-    { header: true, columns: ["key"].concat(languages) , quoted: true },
-    (err, output) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(output);
-      fs.writeFile(translationFilePath + 'translations.csv', '\ufeff' + output, (err) => {
-        if (err) { console.log(err); }
-      });
+  _.map(output, (v, key) => _.merge({ "key": key }, v)),
+  { header: true, columns: ["key"].concat(languages), quoted: true },
+  (err, outData) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(output);
+    fs.writeFile(`${translationFilePath}translations.csv`, `\ufeff${outData}`, (err2) => {
+      if (err2) { console.log(err2); }
     });
+  },
+);
